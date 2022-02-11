aa642c840cf562413bb3ade1d9ac86139c194b00
Nicholas Moen <arcanemachine@tutanota.com>
2022-02-10 23:54:21 -0700

    Create hacky update functions that delete and re-create a record

---
+++ record_update.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from record_create import record_create
+from record_delete import record_delete
+
+record_delete()
+record_create()
---
+++ zone_update.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from zone_create import zone_create
+from zone_delete import zone_delete
+
+zone_delete()
+zone_create()
