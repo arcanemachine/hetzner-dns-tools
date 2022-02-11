63df43ace82eac2ad056ab07be6c9a1066369443
Nicholas Moen <arcanemachine@tutanota.com>
2022-02-11 00:04:53 -0700

    Remove hacky zone/record update shortcuts

--- record_update.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from record_create import record_create
-from record_delete import record_delete
-
-record_delete()
-record_create()
--- zone_update.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-from zone_create import zone_create
-from zone_delete import zone_delete
-
-zone_delete()
-zone_create()
