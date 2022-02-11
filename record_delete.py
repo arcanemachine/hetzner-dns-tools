4924c3ddef917461bbd5e413e5c4b5b741cd9b96
Nicholas Moen <arcanemachine@tutanota.com>
2022-02-10 19:10:42 -0700

    record_delete now works with indirect lookups and multiple records

--- record_delete.py
+++ record_delete.py
@@ -2,24 +2,64 @@
 
 import json
 import os
-import sys
 import requests
+
+from record_get import record_get
 
 import hetzner_dns_helpers as helpers
 
 
+def delete_record_by_id(hetzner_dns_token, record_id):
+    try:
+        response = requests.delete(
+            url=f'https://dns.hetzner.com/api/v1/records/{record_id}',
+            headers={'Auth-API-Token': hetzner_dns_token})
+
+        decoded_response = response.content.decode('utf-8')
+        response_dict = json.loads(decoded_response)
+
+        # check response for errors
+        helpers.check_response_for_errors(response_dict)
+
+        # when running via the terminal, print output to console
+        if __name__ == '__main__':
+            print("OK")
+
+        return "OK"
+
+    except requests.exceptions.RequestException as err:
+        helpers.handle_request_exception(err)
+
+
 def record_delete(hetzner_dns_token=None,
                   record_id=None,
-                  # require_confirmation=False
-                  ):
+                  record_ids=None,
+                  zone_id=None,
+                  zone_name=None,
+                  name=None,
+                  ttl=None,
+                  record_type=None,
+                  value=None,
+                  get_first_record=None,
+                  delete_multiple_records=False,
+                  allow_multiple_zones=False):
     """
     Delete an existing zone.
 
-    - Lookups can be performed directly with 'record_id'.
+    - Deletions can be performed directly with 'record_id', or indirectly
+      using a combination of 'name', 'ttl', 'record_type', 'value',
+      'zone_id', and 'zone_name'.
+
+    - If indirect lookups are performed, an exception will be raised if
+      multiple records are returned.
 
     * hetzner_dns_token *MUST* be passed in args or as environment
       variable (HETZNER_DNS_TOKEN). You can get a DNS API token
       here: https://dns.hetzner.com/settings/api-token
+
+    - If using Bash environment variables, ensure that values are assigned
+      in ALL_CAPS.
+          - e.g. zone_id in Python -> ZONE_ID in environment variable
     """
     if hetzner_dns_token is None:
         # get token from environment variable
@@ -29,35 +69,96 @@ def record_delete(hetzner_dns_token=None,
         # get record_id from environment variable
         record_id = os.environ['RECORD_ID']
 
-    if record_id:
-        try:
-            response = requests.delete(
-                url=f'https://dns.hetzner.com/api/v1/records/{record_id}',
-                headers={'Auth-API-Token': hetzner_dns_token})
+    if record_ids is None and os.environ.get('RECORD_IDS'):
+        # get record_ids from environment variable
+        record_ids = os.environ['RECORD_IDS']
 
-            decoded_response = response.content.decode('utf-8')
-            response_dict = json.loads(decoded_response)
+    if not get_first_record\
+            and os.environ.get('GET_FIRST_RECORD'):
+        # get get_first_record from environment variable
+        get_first_record = os.environ['GET_FIRST_RECORD']
 
-            # check response for errors
-            helpers.check_response_for_errors(response_dict)
+    if not allow_multiple_zones\
+            and os.environ.get('ALLOW_MULTIPLE_ZONES'):
+        # get allow_multiple_zones from environment variable
+        allow_multiple_zones = os.environ['ALLOW_MULTIPLE_ZONES']
 
-            # when running via the terminal, print output to console
-            if __name__ == '__main__':
-                print("OK")
-                sys.exit(0)  # exit successfully
+    if not delete_multiple_records\
+            and os.environ.get('DELETE_MULTIPLE_RECORDS'):
+        # get delete_multiple_records from environment variable
+        delete_multiple_records = os.environ['DELETE_MULTIPLE_RECORDS']
 
-            return "OK"
+    # do an indirect lookup of all relevant records that match the parameters
+    # and allow a single match to be returned
+    if not record_id:
+        if zone_id is None and os.environ.get('ZONE_ID'):
+            # get zone_id from environment variable
+            zone_id = os.environ['ZONE_ID']
 
-        except requests.exceptions.RequestException as err:
-            helpers.handle_request_exception(err)
-    else:
-        error_message = "Must specify record_id"
+        if zone_name is None and os.environ.get('ZONE_NAME'):
+            # get zone_name from environment variable
+            zone_name = os.environ['ZONE_NAME']
 
-        if __name__ == '__main__':
-            print(f"Error: {error_message}")
-            sys.exit(1)  # exit with error
+        if name is None and os.environ.get('NAME'):
+            # get name from environment variable
+            name = os.environ.get('NAME', None)
+
+        if ttl is None and os.environ.get('TTL'):
+            # get ttl from environment variable
+            ttl = int(os.environ['TTL'])
+
+        if record_type is None and os.environ.get('TYPE'):
+            # get record_type from environment variable
+            record_type = os.environ['TYPE']
+
+        if value is None and os.environ.get('VALUE'):
+            # get value from environment variable
+            value = os.environ['VALUE']
+
+        multiple_zones = allow_multiple_zones
+
+        # this method will return a string if one record is returned,
+        # and a list if multiple records are returned
+        record_get_id_result = record_get(zone_id=zone_id,
+                                          zone_name=zone_name,
+                                          name=name,
+                                          ttl=ttl,
+                                          record_type=record_type,
+                                          value=value,
+                                          id_only=True,
+                                          get_first_record=get_first_record,
+                                          allow_multiple_records=True,
+                                          allow_multiple_zones=multiple_zones)
+
+        if type(record_get_id_result) == list:
+            records_count = len(record_get_id_result)
+
+            # do not delete multiple records unless explicitly requested
+            if not delete_multiple_records:
+                error_message = f"Found {records_count} records. To "\
+                    "delete multiple records via a query, assign a truthy "\
+                    "value to 'delete_multiple_records'"
+                helpers.exit_with_error(error_message)
+
+            else:
+                record_ids = record_get_id_result
         else:
-            raise ValueError(error_message)
+            if record_get_id_result:
+                record_id = record_get_id_result
+            else:
+                helpers.exit_with_error("record not found")
+
+    if record_id and record_ids:
+        helpers.exit_with_error(
+            "Cannot use 'record_id' and 'record_ids' at the same time.")
+    elif record_id:
+        delete_record_by_id(hetzner_dns_token, record_id)
+    elif record_ids:
+        for record_id_value in record_ids:
+            delete_record_by_id(hetzner_dns_token, record_id_value)
+    else:
+        helpers.exit_with_error("No 'record_id' or 'record_ids' found.")
+
 
 
 if __name__ == '__main__':
